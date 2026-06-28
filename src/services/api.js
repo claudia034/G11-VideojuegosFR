d1f4570f0554ecaff49aaafb7f88ec447b25e7a2
Pamelalexandragomeztenorio <00139622@uca.edu.sv>
2026-06-28 16:13:10 -0600

    fix: arreglar url a backend

--- src/services/api.js
+++ src/services/api.js
@@ -33,7 +33,7 @@ export const storage = {
 }
 
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: `${API_BASE_URL}/api/v1`,
   headers: {
     'Content-Type': 'application/json'
   }
