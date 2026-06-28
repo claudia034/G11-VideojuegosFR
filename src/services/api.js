add71ab6b53a9d535989c0ad87e9ba9624b48d8b
Pamelalexandragomeztenorio <00139622@uca.edu.sv>
2026-06-28 16:23:34 -0600

    fix: corregir redundancia en construccion de url

--- src/services/api.js
+++ src/services/api.js
@@ -33,7 +33,7 @@ export const storage = {
 }
 
 const axiosInstance = axios.create({
-  baseURL: `${API_BASE_URL}/api/v1`,
+  baseURL: API_BASE_URL,
   headers: {
     'Content-Type': 'application/json'
   }
